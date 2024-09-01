b1b2257d0d964c0d7919972f521898dbbd23eee5
Branimir Amidžić <mail@ambraspace.com>
2024-09-01 16:57:48 +0200

    TextInputDialog fixed

--- src/app/components/dialogs/text-input-dialog/text-input-dialog.component.ts
+++ src/app/components/dialogs/text-input-dialog/text-input-dialog.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 
@@ -13,7 +13,8 @@ import { MatInput } from '@angular/material/input';
     MatFormField,
     MatLabel,
     MatInput,
-    FormsModule
+    FormsModule,
+    MatDialogModule
   ],
   templateUrl: './text-input-dialog.component.html',
   styleUrl: './text-input-dialog.component.css'
@@ -29,7 +30,7 @@ export class TextInputDialog {
     this.categoryName = data.name;
   }
 
-  question: string = "Kategoria";
+  question: string = "Kategorija";
 
   categoryName: string = "";
 
